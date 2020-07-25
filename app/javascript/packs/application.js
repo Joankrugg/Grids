dbc63c88568eb24416d8e7ea1b1044cff66911d0
Philippe chambon <joan_krugg@live.fr>
2020-07-25 12:43:26 +0200

    add design on checkbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,12 @@
 require("@rails/ujs").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+
+window.jQuery = $;
+window.$ = $;
 
 import "bootstrap";
+
+
