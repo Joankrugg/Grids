56796ee10bd274f0bd9c5bb268b6c101babd4fcc
Philippe chambon <joan_krugg@live.fr>
2020-11-07 10:43:43 +0100

    add admin system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get '/user' => "pages#home", :as => :user_root
   root to: 'pages#home'
